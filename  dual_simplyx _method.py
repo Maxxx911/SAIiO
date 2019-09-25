7a2e40febb225d3c3e15773781b01ea3c5e2d8d6
Maxxx911 <maxrikal@gmail.com>
2019-09-25 12:15:33 +0300

    Init class

---
+++  dual_simplyx _method.py
@@ -0,0 +1,15 @@
+class DualSimplyxMethod(object):
+  def __init__(self, A, b, c, d_low, d_high, Jb = None, Jnb = None):
+    self.A = A
+    self.b = b
+    self.c = c
+    self.d_low = d_low
+    self.d_high = d_high
+    self.Jb = Jb
+    self.Jnb = Jnb
+
+def find_basis_indexes(self):
+  if self.Jb == None
+    # Решить первой фазой симплекс метода
+  
+
